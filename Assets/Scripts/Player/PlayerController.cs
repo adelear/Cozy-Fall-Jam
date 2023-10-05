29759ebc9c99684277af3174c4e306de6208da9e
RyanSalam <Ryan@agni.com>
2023-10-05 18:52:51 -0400

    Player Movement Implementation

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Components")]
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float currentCandy = 0;
+    [SerializeField] private float maxCandy = 100;
+    [SerializeField] private AnimationCurve CandyToSpeedCurve;
+
+    Vector2 inputVector;
+
+    private void Awake()
+    {
+        if (rb == null)
+            rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            AddCandy(20);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float t = Mathf.Clamp01(currentCandy / maxCandy);
+        float speed = moveSpeed * CandyToSpeedCurve.Evaluate(t);
+        Vector2 displacement = inputVector * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + displacement);
+    }
+
+    public void AddCandy(float amount)
+    {
+        currentCandy += amount;
+        currentCandy = Mathf.Clamp(currentCandy, 0, maxCandy);
+    }
+
+    public void LoseCandy(float amount)
+    {
+        currentCandy -= amount;
+        currentCandy = Mathf.Clamp(currentCandy, 0, maxCandy);
+    }
+
+    public float GetCurrentCandy()
+    {
+        return currentCandy;
+    }
+}
