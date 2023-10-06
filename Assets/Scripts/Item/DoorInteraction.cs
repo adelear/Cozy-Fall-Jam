93699a64aa6bbc8c282415b91bc65a38dcc6d182
teanbes <teanbes@gmail.com>
2023-10-06 00:22:47 -0400

    Candy Door Interaction

---
+++ Assets/Scripts/Item/DoorInteraction.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorInteraction : MonoBehaviour
+{
+    [SerializeField] private int maxCandyToGive = 5;
+    [SerializeField] private float askForCandyCooldown = 10;
+    [SerializeField] public Slider slider; // temporary HUD Slider
+    private PlayerController playerController;
+    private bool isOnDoor;
+    [SerializeField] private bool canAskForCandy;
+    private float lastTimeAskedCandy;
+    private float currentTime;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = Time.time - lastTimeAskedCandy;
+
+        if (currentTime >= askForCandyCooldown)
+        {
+            canAskForCandy = true;
+        }
+
+        if (isOnDoor && canAskForCandy)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && (currentTime >= askForCandyCooldown))
+            {
+                int candyToGive = Random.Range(1, maxCandyToGive);
+                playerController.AddCandy(candyToGive);
+                canAskForCandy = false;
+                lastTimeAskedCandy = Time.time;
+            }
+        }
+         // this is Temporary HUD
+      
+            UpdateDoorHUDTimer(Mathf.Clamp(((currentTime) / askForCandyCooldown), 0, 1));
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            Debug.Log("Player on door");
+            playerController = other.GetComponent<PlayerController>();
+            isOnDoor = true;
+            canAskForCandy = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            Debug.Log("Player went away");
+            isOnDoor = false;
+        }
+    }
+
+    public void UpdateDoorHUDTimer(float curenthealth)
+    {
+        slider.value = curenthealth;
+    }
+
+}
